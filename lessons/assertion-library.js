2e25607394ffed84cf3003aa4cb049d22b58bdcd
Jan Pozivil <crs1138@me.com>
2022-05-23 20:55:06 +0200

    Recreating basic Jest functionality
    Details:
    ---
    *

--- lessons/assertion-library.js
+++ lessons/assertion-library.js
@@ -1,21 +1,33 @@
-const {sum, subtract} = require('../math')
+const { sum, subtract } = require('../math')
 
-let result, expected
+test('sum test', () => {
+    const result = sum(3, 7)
+    const expected = 10
+    expect(result).toBe(expected)
+})
 
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
+test('subtract test', () => {
+    const result = subtract(7, 3)
+    const expected = 4
+    expect(result).toBe(expected)
+})
 
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+function test(title, callback) {
+    try {
+        callback()
+        console.log(`✅ ${title} passed`)
+    } catch (error) {
+        console.error(`❌ ${title} failed`)
+        console.error(error)
+    }
+}
 
 function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
-      }
+    return {
+        toBe(expected) {
+            if (actual !== expected) {
+                throw new Error(`${actual} is not equal to ${expected}`)
+            }
+        },
     }
-  }
 }
--- setup-globals.js
+++ setup-globals.js
@@ -1,22 +1,21 @@
-async function test(title, callback) {
-  try {
-    await callback()
-    console.log(`✓ ${title}`)
-  } catch (error) {
-    console.error(`✕ ${title}`)
-    console.error(error)
-  }
-}
-
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
-      }
+const test = async (title, callback) => {
+    try {
+        await callback()
+        console.log(`✓ ${title}`)
+    } catch (error) {
+        console.error(`✕ ${title}`)
+        console.error(error)
     }
-  }
 }
 
-global.test = test
+const expect = (actual) => {
+    return {
+        toBe(expected) {
+            if (actual !== expected) {
+                throw new Error(`${actual} is not equal to ${expected}`)
+            }
+        },
+    }
+}
+
 global.expect = expect
